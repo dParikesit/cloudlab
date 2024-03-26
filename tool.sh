fd639827c56eba998c76dca0e7c02840615a7467
Dimas Shidqi Parikesit <dsparikesit@gmail.com>
2024-03-26 13:37:22 +0700

    update

--- tool.sh
+++ tool.sh
@@ -3,11 +3,11 @@
 user=`whoami`
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-. "/users/$user/.cargo/env
-
 curl https://mise.run | sh
 echo "eval \"\$(/users/$user/.local/bin/mise activate bash)\"" >> ~/.bashrc
 
 ~/.local/bin/mise settings set experimental true
-~/.local/bin/mise -y use -g java@temurin-8 maven@3 ant@latest python@2 cargo:cargo-binstall
-~/.local/bin/mise -y use -g cargo:zellij
+~/.local/bin/mise -y use -g java@temurin-8 maven@3 ant@latest python@2 
+
+# ~/.local/bin/mise -y use -g cargo:cargo-binstall
+# ~/.local/bin/mise -y use -g cargo:zellij
