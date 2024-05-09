a4ddce351279ced2f0e7c5a54f40a8824f1db139
Dimas Shidqi Parikesit <dsparikesit@gmail.com>
2024-05-09 07:24:23 +0700

    update

--- tool.sh
+++ tool.sh
@@ -9,7 +9,9 @@ echo "eval \"\$(/home/dimas/.local/bin/mise activate bash)\"" >> ~/.bashrc
 ~/.local/bin/mise settings set experimental true
 ~/.local/bin/mise -y use -g python@2 go@latest
 
-~/.local/bin/mise -y use -g java@temurin-8 maven@3 ant@latest
+~/.local/bin/mise use -g cargo:cargo-binstall
+
+# ~/.local/bin/mise -y use -g java@temurin-8 maven@3 ant@latest
 
 git config --global credential.helper 'store'
 # git -C ~ clone https://github.com/apache/zookeeper.git
@@ -22,6 +24,6 @@ cd ~/zookeeper
 git checkout newbug
 
 # mise use -g cargo:cargo-binstall
-mise use -g cargo:zellij cargo:dust go:github.com/muesli/duf.git
+~/.local/bin/mise use -g cargo:zellij cargo:dust go:github.com/muesli/duf.git
 echo "alias du='dust -r'" >> ~/.bash_aliases
 echo "alias df='duf'" >> ~/.bash_aliases
