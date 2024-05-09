12ea70821772fcad07985e7da830063d53ce973e
Dimas Shidqi Parikesit <dsparikesit@gmail.com>
2024-05-09 14:45:41 +0700

    update

--- tool.sh
+++ tool.sh
@@ -4,7 +4,7 @@ user=`whoami`
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 curl https://mise.run | sh
-echo "eval \"\$(/home/dimas/.local/bin/mise activate bash)\"" >> ~/.bashrc
+echo "eval \"\$(${HOME}/.local/bin/mise activate bash)\"" >> ~/.bashrc
 
 ~/.local/bin/mise settings set experimental true
 ~/.local/bin/mise -y use -g python@2 go@latest
@@ -23,7 +23,8 @@ git -C ~ clone https://github.com/apache/hbase.git
 cd ~/zookeeper
 git checkout newbug
 
-# mise use -g cargo:cargo-binstall
-~/.local/bin/mise use -g cargo:zellij cargo:dust go:github.com/muesli/duf.git
 echo "alias du='dust -r'" >> ~/.bash_aliases
 echo "alias df='duf'" >> ~/.bash_aliases
+
+# mise use -g cargo:cargo-binstall 
+# mise use -g cargo:zellij cargo:dust go:github.com/muesli/duf.git
