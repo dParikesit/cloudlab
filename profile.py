e34a7fb01188ec417812b60fcdd76646072701d4
Dimas Shidqi Parikesit <dsparikesit@gmail.com>
2024-10-17 20:15:52 -0400

    update

--- profile.py
+++ profile.py
@@ -1,18 +1,20 @@
 """Variable number of nodes in a lan. You have the option of picking from one
 of several standard images we provide, or just use the default (typically a recent
 version of Ubuntu). You may also optionally pick the specific hardware type for
-all the nodes in the lan. 
+all the nodes in the lan.
 
 Instructions:
 Wait for the experiment to start, and then log into one or more of the nodes
 by clicking on them in the toplogy, and choosing the `shell` menu option.
-Use `sudo` to run root commands. 
+Use `sudo` to run root commands.
 """
 
 # Import the Portal object.
 import geni.portal as portal
+
 # Import the ProtoGENI library.
 import geni.rspec.pg as pg
+
 # Emulab specific extensions.
 import geni.rspec.emulab as emulab
 
@@ -23,91 +25,177 @@ pc = portal.Context()
 request = pc.makeRequestRSpec()
 
 # Variable number of nodes.
-pc.defineParameter("nodeCount", "Number of Nodes", portal.ParameterType.INTEGER, 1,
-                   longDescription="If you specify more then one node, " +
-                   "we will create a lan for you.")
+pc.defineParameter(
+    "nodeCount",
+    "Number of Nodes",
+    portal.ParameterType.INTEGER,
+    1,
+    longDescription="If you specify more then one node, "
+    + "we will create a lan for you.",
+)
 
 # Pick your OS.
 imageList = [
-    ('default', 'Default Image'),
-    ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD', 'UBUNTU 22.04'),
-    # ('"urn:publicid:IDN+utah.cloudlab.us+image+silentfailure-PG0:t2c-eval"','t2c-eval')
+    ("default", "Default Image"),
+    ("urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD", "UBUNTU 22.04"),
+    ("urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU20-64-STD", "UBUNTU 20.04"),
 ]
 
-nodes = []
-
-pc.defineParameter("osImage", "Select OS image",
-                   portal.ParameterType.IMAGE,
-                   imageList[0], imageList,
-                   longDescription="Most clusters have this set of images, " +
-                   "pick your favorite one.")
+pc.defineParameter(
+    "osImage",
+    "Select OS image",
+    portal.ParameterType.IMAGE,
+    imageList[0],
+    imageList,
+    longDescription="Most clusters have this set of images, "
+    + "pick your favorite one.",
+)
 
 # Optional physical type for all nodes.
-pc.defineParameter("phystype",  "Optional physical node type",
-                   portal.ParameterType.STRING, "",
-                   longDescription="Specify a single physical node type (c6525-25g,d710,etc) " +
-                   "instead of letting the resource mapper choose for you.")
+pc.defineParameter(
+    "phystype",
+    "Optional physical node type",
+    portal.ParameterType.NODETYPE,
+    "",
+    longDescription="Pick a single physical node type (pc3000,d710,etc) "
+    + "instead of letting the resource mapper choose for you.",
+)
+
+# Optionally create XEN VMs instead of allocating bare metal nodes.
+pc.defineParameter(
+    "useVMs",
+    "Use XEN VMs",
+    portal.ParameterType.BOOLEAN,
+    False,
+    longDescription="Create XEN VMs instead of allocating bare metal nodes.",
+)
+
+# Optionally start X11 VNC server.
+pc.defineParameter(
+    "startVNC",
+    "Start X11 VNC on your nodes",
+    portal.ParameterType.BOOLEAN,
+    False,
+    longDescription="Start X11 VNC server on your nodes. There will be "
+    + "a menu option in the node context menu to start a browser based VNC "
+    + "client. Works really well, give it a try!",
+)
 
 # Optional link speed, normally the resource mapper will choose for you based on node availability
-pc.defineParameter("linkSpeed", "Link Speed",portal.ParameterType.INTEGER, 0,
-                   [(0,"Any"),(100000,"100Mb/s"),(1000000,"1Gb/s"),(10000000,"10Gb/s"),(25000000,"25Gb/s"),(100000000,"100Gb/s")],
-                   advanced=True,
-                   longDescription="A specific link speed to use for your lan. Normally the resource " +
-                   "mapper will choose for you based on node availability and the optional physical type.")
-                   
+pc.defineParameter(
+    "linkSpeed",
+    "Link Speed",
+    portal.ParameterType.INTEGER,
+    0,
+    [
+        (0, "Any"),
+        (100000, "100Mb/s"),
+        (1000000, "1Gb/s"),
+        (10000000, "10Gb/s"),
+        (25000000, "25Gb/s"),
+        (100000000, "100Gb/s"),
+    ],
+    advanced=True,
+    longDescription="A specific link speed to use for your lan. Normally the resource "
+    + "mapper will choose for you based on node availability and the optional physical type.",
+)
+
 # For very large lans you might to tell the resource mapper to override the bandwidth constraints
 # and treat it a "best-effort"
-pc.defineParameter("bestEffort",  "Best Effort", portal.ParameterType.BOOLEAN, False,
-                    advanced=True,
-                    longDescription="For very large lans, you might get an error saying 'not enough bandwidth.' " +
-                    "This options tells the resource mapper to ignore bandwidth and assume you know what you " +
-                    "are doing, just give me the lan I ask for (if enough nodes are available).")
-                    
+pc.defineParameter(
+    "bestEffort",
+    "Best Effort",
+    portal.ParameterType.BOOLEAN,
+    False,
+    advanced=True,
+    longDescription="For very large lans, you might get an error saying 'not enough bandwidth.' "
+    + "This options tells the resource mapper to ignore bandwidth and assume you know what you "
+    + "are doing, just give me the lan I ask for (if enough nodes are available).",
+)
+
 # Sometimes you want all of nodes on the same switch, Note that this option can make it impossible
 # for your experiment to map.
-pc.defineParameter("sameSwitch",  "No Interswitch Links", portal.ParameterType.BOOLEAN, False,
-                    advanced=True,
-                    longDescription="Sometimes you want all the nodes connected to the same switch. " +
-                    "This option will ask the resource mapper to do that, although it might make " +
-                    "it imppossible to find a solution. Do not use this unless you are sure you need it!")
+pc.defineParameter(
+    "sameSwitch",
+    "No Interswitch Links",
+    portal.ParameterType.BOOLEAN,
+    False,
+    advanced=True,
+    longDescription="Sometimes you want all the nodes connected to the same switch. "
+    + "This option will ask the resource mapper to do that, although it might make "
+    + "it imppossible to find a solution. Do not use this unless you are sure you need it!",
+)
 
 # Optional ephemeral blockstore
-pc.defineParameter("tempFileSystemSize", "Temporary Filesystem Size",
-                   portal.ParameterType.INTEGER, 0,advanced=True,
-                   longDescription="The size in GB of a temporary file system to mount on each of your " +
-                   "nodes. Temporary means that they are deleted when your experiment is terminated. " +
-                   "The images provided by the system have small root partitions, so use this option " +
-                   "if you expect you will need more space to build your software packages or store " +
-                   "temporary files.")
-                   
-# Instead of a size, ask for all available space. 
-pc.defineParameter("tempFileSystemMax",  "Temp Filesystem Max Space",
-                    portal.ParameterType.BOOLEAN, False,
-                    advanced=True,
-                    longDescription="Instead of specifying a size for your temporary filesystem, " +
-                    "check this box to allocate all available disk space. Leave the size above as zero.")
+pc.defineParameter(
+    "tempFileSystemSize",
+    "Temporary Filesystem Size",
+    portal.ParameterType.INTEGER,
+    0,
+    advanced=True,
+    longDescription="The size in GB of a temporary file system to mount on each of your "
+    + "nodes. Temporary means that they are deleted when your experiment is terminated. "
+    + "The images provided by the system have small root partitions, so use this option "
+    + "if you expect you will need more space to build your software packages or store "
+    + "temporary files.",
+)
 
-pc.defineParameter("tempFileSystemMount", "Temporary Filesystem Mount Point",
-                   portal.ParameterType.STRING,"/mydata",advanced=True,
-                   longDescription="Mount the temporary file system at this mount point; in general you " +
-                   "you do not need to change this, but we provide the option just in case your software " +
-                   "is finicky.")
+# Instead of a size, ask for all available space.
+pc.defineParameter(
+    "tempFileSystemMax",
+    "Temp Filesystem Max Space",
+    portal.ParameterType.BOOLEAN,
+    False,
+    advanced=True,
+    longDescription="Instead of specifying a size for your temporary filesystem, "
+    + "check this box to allocate all available disk space. Leave the size above as zero.",
+)
+
+pc.defineParameter(
+    "tempFileSystemMount",
+    "Temporary Filesystem Mount Point",
+    portal.ParameterType.STRING,
+    "/mydata",
+    advanced=True,
+    longDescription="Mount the temporary file system at this mount point; in general you "
+    + "you do not need to change this, but we provide the option just in case your software "
+    + "is finicky.",
+)
+
+pc.defineParameter(
+    "exclusiveVMs",
+    "Force use of exclusive VMs",
+    portal.ParameterType.BOOLEAN,
+    True,
+    advanced=True,
+    longDescription="When True and useVMs is specified, setting this will force allocation "
+    + "of exclusive VMs. When False, VMs may be shared or exclusive depending on the policy "
+    + "of the cluster.",
+)
 
 # Retrieve the values the user specifies during instantiation.
 params = pc.bindParameters()
 
 # Check parameter validity.
 if params.nodeCount < 1:
-    pc.reportError(portal.ParameterError("You must choose at least 1 node.", ["nodeCount"]))
+    pc.reportError(
+        portal.ParameterError("You must choose at least 1 node.", ["nodeCount"])
+    )
 
 if params.tempFileSystemSize < 0 or params.tempFileSystemSize > 200:
-    pc.reportError(portal.ParameterError("Please specify a size greater then zero and " +
-                                         "less then 200GB", ["tempFileSystemSize"]))
+    pc.reportError(
+        portal.ParameterError(
+            "Please specify a size greater then zero and " + "less then 200GB",
+            ["tempFileSystemSize"],
+        )
+    )
 
 if params.phystype != "":
     tokens = params.phystype.split(",")
     if len(tokens) != 1:
-        pc.reportError(portal.ParameterError("Only a single type is allowed", ["phystype"]))
+        pc.reportError(
+            portal.ParameterError("Only a single type is allowed", ["phystype"])
+        )
 
 pc.verifyParameters()
 
@@ -129,9 +217,16 @@ if params.nodeCount > 1:
 # Process nodes, adding to link or lan.
 for i in range(params.nodeCount):
     # Create a node and add it to the request
-    name = "node" + str(i)
-    node = request.RawPC(name)
-    nodes.append(node)
+    if params.useVMs:
+        name = "vm" + str(i)
+        node = request.XenVM(name)
+        if params.exclusiveVMs:
+            node.exclusive = True
+            pass
+    else:
+        name = "node" + str(i)
+        node = request.RawPC(name)
+        pass
     if params.osImage and params.osImage != "default":
         node.disk_image = params.osImage
         pass
@@ -154,6 +249,16 @@ for i in range(params.nodeCount):
             pass
         bs.placement = "any"
         pass
+    #
+    # Install and start X11 VNC. Calling this informs the Portal that you want a VNC
+    # option in the node context menu to create a browser VNC client.
+    #
+    # If you prefer to start the VNC server yourself (on port 5901) then add nostart=True.
+    #
+    if params.startVNC:
+        node.startVNC()
+        pass
+    pass
 
 for node in nodes:
     node.addService(pg.Install(
